a17c3d1ad027a9ed731290367e36c2dbd15362d2
midoarbi <elmehdinassiri31@gmail.com>
2019-03-25 00:18:23 +0100

    Add All Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/form';
+import Weather from './components/weather';
+import Weatherstate from './components/watherstate';
+
+const API_KEY = "e06fffabed6b4c691db16eb844091f45";
+
+//http://api.openweathermap.org/data/2.5/weather?q=cairo,egypt&appid=e36ed364400282e43250b6c4c0274d44
 
 class App extends Component {
+
+  state = {
+    tempreature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error : ''
+  }
+
+  getWeather = async (e) =>
+  {
+   e.preventDefault();
+
+   const city = e.target.elements.city.value;
+   const country = e.target.elements.country.value;
+   const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+   const data = await api.json();
+   console.log(data);
+
+   if(city && country)
+   {
+     this.setState({
+      tempreature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error : ''
+     })
+   }
+   else
+   {
+    this.setState({
+      tempreature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error : 'please type data to get weather result! '
+    })
+   }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Wrapper">
+      <div className="form-container">
+      <Weatherstate />
+      <Form getWeather={this.getWeather} />
+      <Weather 
+    tempreature = {this.state.tempreature}
+    city = {this.state.city}
+    country = {this.state.country}
+    humidity = {this.state.humidity}
+    description = {this.state.description}
+    error = {this.state.error}
+      />
+
+      </div>
       </div>
     );
   }
---
+++ src/components/watherstate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Weatherstate = () =>
+{
+    return(
+<div>
+<img className="img-weather"
+src="https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/cloudy-512.png"
+alt="wather"
+/>
+</div>
+    );
+}
+export default Weatherstate;
